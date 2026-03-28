44d1ecd0e4ae20eaa47ea41c6917854aa04a9472
Sayantan Chatterjee <sayantanchatt44@outlook.com>
2026-03-28 20:20:41 +0530

    Text to speech module is ready

--- main.py
+++ main.py
@@ -1,2 +1,58 @@
+import speech_recognition as sr
+import os
+import threading
+from mtranslate import translate
+from colorama import Fore,Style,init
+
+init(autoreset=True)
+
+def print_loop():
+    while True:
+        print(Fore.GREEN + "Listening...", end="", flush=True)
+        print(Style.RESET_ALL, end="",flush=True)
+
+def translate_hi_to_en(text):
+    en_text=translate(text, "en-us")
+    return en_text
+
+def Speech_to_text_py():
+    recognizer= sr.Recognizer()
+    recognizer.dynamic_energy_threshold = False
+    recognizer.energy_threshold = 34000
+    recognizer.dynamic_energy_adjustment_damping = 0.010
+    recognizer.dynamic_energy_ratio = 1.0
+    recognizer.pause_threshold= 0.9
+    recognizer.operation_timeout=None
+    recognizer.pause_threshold=0.5
+    recognizer.non_speaking_duration=0.5
+
+    with sr.Microphone() as source:
+        recognizer.adjust_for_ambient_noise(source)
+        while True:
+            print(Fore.GREEN + "Listening...", end="", flush=True)
+            try:
+                audio = recognizer.listen(source, timeout=None)
+                print("\r" + Fore.CYAN + "Recognizing...", end="", flush=True)
+                recognized_text=recognizer.recognize_google(audio).lower()
+                if recognized_text:
+                    text_output= translate_hi_to_en(str(recognized_text))
+                    print("\r" + Fore.LIGHTBLUE_EX + text_output)
+                    return recognized_text
+                else:
+                    return ""
+            except sr.UnknownValueError:
+                recognized_text="Sorry! Not recognized"
+            finally:
+                print("\r", end="", flush=True)
+            
+            os.system("cls" if os.name == "jv" else "clear")
+        
+        stt_thread=threading.Thread(target=Speech_to_text_py)
+        print_thread=threading.Thread(target=print_loop)
+        stt_thread.start()
+        stt_thread.join()
+        print_loop.join()
+
+
 def main():
-    print("Hello Jarvis")
+    Speech_to_text_py()
