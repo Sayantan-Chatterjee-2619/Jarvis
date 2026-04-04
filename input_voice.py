de9a218456a5cc7cd76096587aaad2158ef78e82
Sayantan Chatterjee <117353748+Sayantan-Chatterjee-2619@users.noreply.github.com>
2026-04-04 21:26:24 +0530

    Code Changed in input_voice

--- input_voice.py
+++ input_voice.py
@@ -1,16 +1,19 @@
 import speech_recognition as sr
 import os
 import threading
+import sys
 from mtranslate import translate
 from colorama import Fore,Style,init
 from output_voice import translate_en_to_hi
 
 init(autoreset=True)
 
-def print_loop():
-    while True:
-        print(Fore.GREEN + "Listening...", end="", flush=True)
-        print(Style.RESET_ALL, end="",flush=True)
+stop_listening = threading.Event()
+
+def clear_terminal():
+    # ANSI escape code to clear screen and move cursor to top-left
+    sys.stdout.write("\033[2J\033[H")
+    sys.stdout.flush()
 
 def translate_hi_to_en(text):
     en_text=translate(text, "en-us")
@@ -29,32 +32,31 @@ def Speech_to_text_py():
 
     with sr.Microphone() as source:
         recognizer.adjust_for_ambient_noise(source)
-        while True:
-            print(Fore.GREEN + "Listening...", end="", flush=True)
+        while not stop_listening.is_set():   # check stop flag
+            print(Fore.GREEN + "Listening..." + Style.RESET_ALL, end="", flush=True)
             try:
                 audio = recognizer.listen(source, timeout=None)
-                print("\r" + Fore.CYAN + "Recognizing...", end="", flush=True)
-                recognized_text=recognizer.recognize_google(audio).lower()
+                print("\r" + Fore.CYAN + "Recognizing..." + Style.RESET_ALL, end="", flush=True)
+                recognized_text = recognizer.recognize_google(audio).lower()
                 if recognized_text:
-                    text_output= translate_hi_to_en(str(recognized_text))
+                    if recognized_text.strip() == "exit":
+                        print("\r" + Fore.RED + "Exit command detected. Stopping..." + Style.RESET_ALL)
+                        stop_listening.set()   # signal stop
+                        break
+                    text_output = translate_hi_to_en(recognized_text)
                     translate_en_to_hi(text_output)
-                    print("\r" + Fore.LIGHTBLUE_EX + text_output)
-                    return text_output
+                    print("\r" + Fore.LIGHTBLUE_EX + text_output + Style.RESET_ALL)
+                    clear_terminal()
                 else:
-                    return ""
+                    print("\rNo speech detected.")
             except sr.UnknownValueError:
-                recognized_text="Sorry! Not recognized"
+                print("\rSorry! Not recognized")
             finally:
-                print("\r", end="", flush=True)
-            
-            os.system("cls" if os.name == "jv" else "cls")
+                clear_terminal()
         
             stt_thread=threading.Thread(target=Speech_to_text_py)
-            print_thread=threading.Thread(target=print_loop)
-            print(print_thread)
             stt_thread.start()
             stt_thread.join()
-            print_loop.join()
         
     
 
--- output_voice.py
+++ output_voice.py
@@ -3,7 +3,6 @@ import os
 import threading
 import asyncio
 from mtranslate import translate
-from input_voice import Speech_to_text_py
 
 import edge_tts
 
@@ -63,7 +62,5 @@ def speak(text, output_file=None):
     # Play audio in a separate thread
     thread = threading.Thread(target=play_audio, args=(output_file,))
     thread.start()
-    thread.join()
-
-    Speech_to_text_py()
+    thread.join()    
 
